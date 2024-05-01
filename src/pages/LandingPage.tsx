856d940a3894319a3eb1588d562ef72f6526ea80
Eric Chang <fsc8504@gmail.com>
2024-04-30 19:54:19 -0700

    Add landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import LandingMain from "../components/landing/LandingMain";
+
+function LnadingPage() {
+    return (
+        <div>
+            <LandingMain />
+        </div>
+    );
+}
+
+export default LnadingPage;

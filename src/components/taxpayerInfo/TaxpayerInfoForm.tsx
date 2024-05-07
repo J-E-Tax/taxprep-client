3c53d5a8c57548ab15f6614cfb0d6843b81368a8
Eric Chang <fsc8504@gmail.com>
2024-05-07 13:06:35 -0700

    Edit taxpayer info

--- src/components/taxpayerInfo/TaxpayerInfoForm.tsx
+++ src/components/taxpayerInfo/TaxpayerInfoForm.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useEffect } from 'react';
-import { Button, Form, TextInput, FormGroup, Label, GridContainer, Grid } from '@trussworks/react-uswds';
+import { Button, Form, TextInput, FormGroup, Label, GridContainer, Grid, TextInputMask } from '@trussworks/react-uswds';
 import { createTaxpayerInfo, updateTaxpayerInfo } from '../../api/taxpayerApi';
 import { getTaxpayerInfo } from '../../api/taxpayerApi';
 import { setTaxpayerInfo } from '../../features/taxpayerInfo/taxpayerInfoSlice';
@@ -17,7 +17,7 @@ function TaxpayerInfoForm () {
             .then((res) => {
                 dispatch(setTaxpayerInfo({
                     ...res.data,
-                    dob : res.data.dob ? new Date(res.data.dob).toISOString().split('T')[0] : "", // FORMAT dob
+                    // dob : res.data.dob ? new Date(res.data.dob).toISOString().split('T')[0] : "", // FORMAT dob
                     address: res.data.address ? JSON.parse(res.data.address) : ""}));
             })
             .catch((err) => console.error(err));
@@ -38,6 +38,7 @@ function TaxpayerInfoForm () {
         dispatch(setTaxpayerInfo(updateTaxpayerInfo));
     };
 
+
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
         // need to serialize address object to JSON string before sending to API
@@ -48,23 +49,28 @@ function TaxpayerInfoForm () {
 
         if (taxpayerInfo.userId) {
             updateTaxpayerInfo(payload)
-                .then((res: any) => console.log(res))
+                .then((res: any) => {
+                    console.log(res);
+                    dispatch(setTaxpayerInfo({ userId: res.data.userId }));
+                })
                 .catch((err: any) => console.error('Failed updating taxpayer info:', err));
         } else {
             createTaxpayerInfo(payload)
                 .then((res: { data: { userId: any; }; }) => {
                     console.log(res)
-                    dispatch(setTaxpayerInfo({ ...taxpayerInfo, userId: res.data.userId }));
+                    dispatch(setTaxpayerInfo({ userId: res.data.userId }));
                 })
                 .catch((err: any) => console.error('Failed creating taxpayer info:', err));
         }
     };
 
     return   (
-        <div className="bg-base-lightest">
+        <div className="bg-white">
+
         <GridContainer className="usa-section">
+
             <Grid row className="margin-x-neg-05 flex-justify-center">
-                <div className="bg-white padding-y-3 padding-x-15 border border-base-lighter">
+                <div className="bg-white padding-y-3 padding-x-15 border border-base-lighter" style={{ boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' }}>
                     <h2>Tell us about yourself</h2>
                     <Form onSubmit={handleSubmit} className="usa-form usa-form--large margin-bottom-3">
                         <Grid row gap>
@@ -77,6 +83,8 @@ function TaxpayerInfoForm () {
                                         type="text"
                                         value={taxpayerInfo.firstName}
                                         onChange={handleChange}
+                                        pattern="[A-Za-z]+"
+                                        title="First name should only contain letters."
                                     />
                                 </FormGroup>
                             </Grid>
@@ -89,6 +97,8 @@ function TaxpayerInfoForm () {
                                         type="text"
                                         value={taxpayerInfo.middleName}
                                         onChange={handleChange}
+                                        pattern="[A-Za-z]+"
+                                        title="Middle name should only contain letters."
                                     />
                                 </FormGroup>
                             </Grid>
@@ -104,6 +114,8 @@ function TaxpayerInfoForm () {
                                         type="text"
                                         value={taxpayerInfo.lastName}
                                         onChange={handleChange}
+                                        pattern="[A-Za-z]+"
+                                        title="Last name should only contain letters."
                                     />
                                 </FormGroup>
                             </Grid>
@@ -125,10 +137,14 @@ function TaxpayerInfoForm () {
                             <Grid col={6}>
                                 <FormGroup>
                                     <Label htmlFor="ssn">Social Security Number</Label>
-                                    <TextInput
+                                    <TextInputMask
                                         id='ssn'
                                         name='ssn'
                                         type='text'
+                                        mask="___ __ ____"
+                                        pattern="^(?!(000|666|9))\d{3} (?!00)\d{2} (?!0000)\d{4}$"
+                                        aria-labelledby="ssn"
+                                        aria-describedby="hint-ssn"
                                         value={taxpayerInfo.ssn}
                                         onChange={handleChange}
                                     />
@@ -153,10 +169,14 @@ function TaxpayerInfoForm () {
                             <Grid col={6}>
                                 <FormGroup>
                                     <Label htmlFor="phoneNumber">Phone Number</Label>
-                                    <TextInput
+                                    <TextInputMask
                                         id="phoneNumber"
                                         name="phoneNumber"
                                         type="text"
+                                        mask="###-###-####"
+                                        pattern="^\d{3}-\d{3}-\d{4}$"
+                                        aria-labelledby="phoneNumber"
+                                        aria-describedby="hint-phoneNumber"
                                         value={taxpayerInfo.phoneNumber}
                                         onChange={handleChange}
                                     />
@@ -186,6 +206,8 @@ function TaxpayerInfoForm () {
                                         type="text"
                                         value={taxpayerInfo.address.city}
                                         onChange={handleChange}
+                                        pattern="[A-Za-z]+"
+                                        title="State should only contain letters."
                                     />
                                 </FormGroup>
                             </Grid>
@@ -198,6 +220,8 @@ function TaxpayerInfoForm () {
                                         type="text"
                                         value={taxpayerInfo.address.state}
                                         onChange={handleChange}
+                                        pattern="[A-Za-z]+"
+                                        title="State should only contain letters."
                                     />
                                 </FormGroup>
                             </Grid>
@@ -205,11 +229,15 @@ function TaxpayerInfoForm () {
 
                         <FormGroup>
                             <Label htmlFor="zip">ZIP Code</Label>
-                            <TextInput
+                            <TextInputMask
                                 id="zip"
                                 name="address.zip"
                                 type="text"
                                 inputSize="medium"
+                                mask="#####-####"
+                                pattern="^[0-9]{5}(?:-[0-9]{4})?$"
+                                aria-labelledby="zip"
+                                aria-describedby="hint-zip"
                                 value={taxpayerInfo.address.zip}
                                 onChange={handleChange}
                             />
--- src/pages/TaxpayerInfoPage.tsx
+++ src/pages/TaxpayerInfoPage.tsx
@@ -1,15 +1,42 @@
 
+// import { StepIndicator, StepIndicatorStep } from '@trussworks/react-uswds';
 import TaxpayerInfoForm from '../components/taxpayerInfo/TaxpayerInfoForm';
-import './pageStyle.css';
+// import styles from './TaxpayerInfoPageStyle.module.css';
 
 function TaxpayerInfoPage() {
 
     return (
-        <div className="bg-base-lightest">
-            <div className="page-container">
-                <TaxpayerInfoForm />
+        <>
+            <div>
+
+                <div className="page-container">
+                    {/* <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            width: '100%',
+                        }}>
+                        <StepIndicator
+                                className={styles.stepIndicator}
+                                headingLevel="h4"
+                                ofText="of"
+                                stepText="Step"
+                            >
+                            <StepIndicatorStep
+                                label="Personal information"
+                                status="current"
+                            />
+                            <StepIndicatorStep
+                                label="Supporting documents"
+                            />
+                            <StepIndicatorStep label="Review" />
+                            <StepIndicatorStep label="Results" />
+                        </StepIndicator>
+
+                        </div> */}
+                    <TaxpayerInfoForm />
+                </div>
             </div>
-        </div>
+        </>
     );
 
 }

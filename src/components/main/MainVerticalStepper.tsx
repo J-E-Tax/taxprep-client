8e0ec4b0d5d6e18b3e9924b9f1f22ef09ae47dc4
Eric Chang <fsc8504@gmail.com>
2024-05-07 13:01:08 -0700

    Edit main page

--- src/components/main/MainVerticalStepper.tsx
+++ src/components/main/MainVerticalStepper.tsx
@@ -1,6 +1,7 @@
 import { SetStateAction, useState } from 'react';
 import { Button } from '@trussworks/react-uswds';
 import { To, useNavigate } from 'react-router-dom';
+import "./MainVerticalStepperStyle.css";
 
 function MainVerticalStepper() {
 
@@ -17,35 +18,30 @@ function MainVerticalStepper() {
 
   const steps = [
     { name: 'Personal Info', content: 'Detail your personal information.  ', path: '/taxpayer-info' },
-    { name: 'Wages & Income', content: 'Detail your wages and income sources.' , path: '/taxpayer-info'},
+    { name: 'Wages & Income', content: 'Detail your wages and income sources.' , path: '/taxes'},
     { name: 'Review', content: 'Review your entries.' , path: '/taxpayer-info'},
     { name: 'Results', content: 'Finish and file your taxes.', path: '/results' }
   ];
 
   return (
-    <div style={
-      {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '3rem',
-      }
-    }>
-        <ul className="usa-process-list">
-        {steps.map((step, i) => (
-          <li key={i} className={`usa-process-list__item ${i === activeStep ? 'usa-process-list__item--current' : ''}`} style={{width: '300px'}}>
-            <h4 className="usa-process-list__heading" onClick={() => handleStepClick(i)}>{step.name}</h4>
-            {i === activeStep && (
-              <div>
-                <p>{step.content}</p>
-                <Button type="button" onClick={() => handleButtonClick(step.path)}>
-                  {i === steps.length - 1 ? 'Finish' : 'Edit/Review'}</Button>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
+    <div className="stepper-container">
+      {steps.map((step, i) => (
+        <div
+          key={i}
+          className={`card ${i === activeStep ? 'active' : ''}`}
+          onClick={() => handleStepClick(i)}
+        >
+          <h4>{step.name}</h4>
+          {i === activeStep && (
+            <>
+              <p>{step.content}</p>
+              <Button type="button"  onClick={() => handleButtonClick(step.path)}>
+                {i === steps.length - 1 ? 'Finish' : 'Edit/Review'}
+              </Button>
+            </>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,33 @@
+import Lottie from "lottie-react";
 import MainVerticalStepper from "../components/main/MainVerticalStepper";
+import robotAnimation from "../components/main/robot.json";
 
 function MainPage() {
   return (
-    <div className="bg-base-lightest">
-      <MainVerticalStepper />
+    <div className="bg-base-lightest" style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: '100vh',
+      paddingTop: '5rem',
+    }}>
+      <h2>What Would You Like To Do?</h2>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+      }}>
+        <Lottie
+          animationData={robotAnimation}
+          style={{ height: 200 }}
+        />
+        <MainVerticalStepper />
+        <Lottie
+          animationData={robotAnimation}
+          style={{ height: 200 }}
+        />
+      </div>
     </div>
   );
 }

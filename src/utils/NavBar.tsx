4432178712332c3e1ea004e133d64724c725d363
Eric Chang <fsc8504@gmail.com>
2024-05-07 22:49:16 -0700

    Add overall style and animation

--- src/utils/NavBar.tsx
+++ src/utils/NavBar.tsx
@@ -1,45 +1,49 @@
-    import {  useNavigate } from 'react-router-dom';
-    import { Header, GridContainer, PrimaryNav } from '@trussworks/react-uswds';
-    import { useDispatch, useSelector } from 'react-redux';
-    import {logout } from '../features/auth/authSlice';
-    import { RootState } from '../app/store';
+import {  useNavigate } from 'react-router-dom';
+import { Header, GridContainer, PrimaryNav } from '@trussworks/react-uswds';
+import { useDispatch, useSelector } from 'react-redux';
+import {logout } from '../features/auth/authSlice';
+import { RootState } from '../app/store';
+import Lottie from "lottie-react";
+import JoeAndEricLogo from "./JoeAndEricLogo.json";
 
-    function NavBar() {
-        const navigate = useNavigate();
-        const dispatch = useDispatch();
+function NavBar() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-        const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
-        const handleLogout = () => {
-            dispatch(logout());
-            navigate('/login');
-        };
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/login');
+    };
 
-        const items = [
-            <a href="/main" key="home">Home</a>,
-            <a href="/taxpayer-info" key="personalInfo">Personal Info</a>,
-            <a href="/taxes" key="taxInfo">Tax Info</a>,
-            <a href="" key="review">Review</a>,
-            <a href="/results" key="taxResults">Tax Results</a>,
-            <button onClick={handleLogout} key="logout">Logout</button>
-        ];
+    const items = [
+        <a href="/main" key="home">Home</a>,
+        <a href="/taxpayer-info" key="personalInfo">Personal Info</a>,
+        <a href="/taxes" key="taxInfo">Tax Info</a>,
+        <a href="" key="review">Review</a>,
+        <a href="/results" key="taxResults">Tax Results</a>,
+        <button onClick={handleLogout} key="logout">Logout</button>
+    ];
 
-        return (
-            <div style={{ borderBottom: '1px solid #808080' }}>
-                <Header basic={true}>
-                    <GridContainer>
-                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                            { isAuthenticated  &&
-                            <>
-                                <h4>TaxPrep</h4>
-                                <PrimaryNav items={items} />
-                            </>
-                            }
-                        </div>
-                    </GridContainer>
-                </Header>
-            </div>
-        );
-    }
+    return (
+        <div style={{ borderBottom: '1px solid #808080' }}>
+            <Header basic={true}>
+                <GridContainer>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                        { isAuthenticated  &&
+                        <>
+                            <a href="/" title="Home" aria-label="Home">
+                                <Lottie animationData={JoeAndEricLogo}  style={{ width: "150px" }} />
+                            </a>
+                            <PrimaryNav items={items} />
+                        </>
+                        }
+                    </div>
+                </GridContainer>
+            </Header>
+        </div>
+    );
+}
 
-    export default NavBar;
+export default NavBar;

491a5339fe4ce631b26af8d0de2e0ae2e2393202
Eric Chang <fsc8504@gmail.com>
2024-05-09 11:07:12 -0700

    Edit nav bar for route protection and language selector

--- src/utils/NavBar.tsx
+++ src/utils/NavBar.tsx
@@ -1,5 +1,6 @@
-import {  useNavigate } from 'react-router-dom';
-import { Header, GridContainer, PrimaryNav } from '@trussworks/react-uswds';
+import {  useNavigate, useLocation  } from 'react-router-dom';
+import { Header, GridContainer, PrimaryNav, LanguageSelector } from '@trussworks/react-uswds';
+import i18next from 'i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import {logout } from '../features/auth/authSlice';
 import { RootState } from '../app/store';
@@ -9,24 +10,52 @@ import JoeAndEricLogo from "./JoeAndEricLogo.json";
 function NavBar() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const location = useLocation();
 
     const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
-
     const handleLogout = () => {
         dispatch(logout());
-        navigate('/login');
+        navigate('/');
     };
 
     const items = [
         <a href="/main" key="home">Home</a>,
         <a href="/taxpayer-info" key="personalInfo">Personal Info</a>,
         <a href="/UserTaxes" key="taxInfo">Tax Info</a>,
-        <a href="" key="review">Review</a>,
-        <a href="/results" key="taxResults">Tax Results</a>,
+        <a href="/UserTaxes" key="review">Review</a>,
+        <LanguageSelector
+            key="languageSelector"
+            label="Languages"
+            langs={[
+                {
+                    attr: 'zh',
+                    label: '简体字',
+                    label_local: 'Chinese - Simplified',
+                    on_click: () => i18next.changeLanguage('zh')
+                },
+                {
+                    attr: 'en',
+                    label: 'English',
+                    on_click: () => i18next.changeLanguage('en')
+                },
+                {
+                    attr: 'es',
+                    label: 'Español',
+                    label_local: 'Spanish',
+                    on_click: () => i18next.changeLanguage('es')
+                }
+            ]}
+        />,
         <button onClick={handleLogout} key="logout">Logout</button>
     ];
+    // These are the paths that the NavBar will only be shown on
+    const visiblePaths = ['/main', '/results', '/UserTaxes', '/taxpayer-info', '/taxes/:formType/:formID', '/taxes/:formType', '/UserTaxes'];
 
+    // If the path user is on isnot in the visiblePaths, return null
+    if (!visiblePaths.includes(location.pathname)) {
+        return null;
+    }
 
     return (
         <div style={{ borderBottom: '1px solid #808080' }}>
@@ -38,7 +67,7 @@ function NavBar() {
                             <a href="/" title="Home" aria-label="Home">
                             <Lottie animationData={JoeAndEricLogo}  style={{ width: "150px" }} />
                             </a>
-                            <PrimaryNav items={items} />
+                            <PrimaryNav items={items}  />
                         </>
                         }
                     </div>
